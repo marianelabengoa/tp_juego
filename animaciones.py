35dd4da5e0b9991d3a3a2fbe5d8aadf82ff87daf
marianelabengoa <marianelaluzbengoa@gmail.com>
2023-07-11 20:37:14 -0300

    tp juego

--- animaciones.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-
-def get_animations(size):
-    animaciones = [pygame.transform.scale(pygame.image.load("./assets/imagen/fondo/primer_portal.png").convert_alpha(), size), pygame.transform.scale(pygame.image.load("assets/imagen/fondo/segundo_portal.png").convert_alpha(
-    ), size), pygame.transform.scale(pygame.image.load("./assets/imagen/fondo/tercer_portal.png").convert_alpha(), size), pygame.transform.scale(pygame.image.load("assets/imagen/fondo/segundo_portal.png").convert_alpha(), size)]
-    return animaciones
